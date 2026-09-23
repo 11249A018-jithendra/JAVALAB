246acb002f93483e03ef872d842ff9bade158ac7
Jithendra <11249a018@kanchiuniv.ac.in>
2026-09-23 16:48:39 +0000

    added files

---
+++ EXP1/printmarksabove.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class MarksAbvsixty {
+    public static void main(String args[]) {
+        int marks[] = new int[6];
+        String name[] = new String[6];
+        Scanner scanner = new Scanner(System.in);
+        // Input student names and marks
+        for (int i = 0; i < 6; i++) {
+            System.out.print("Enter Name of Student and Marks of Subject " + (i + 1) + ": ");
+            name[i] = scanner.next();
+            marks[i] = scanner.nextInt();
+        }
+        // Display students who scored 60 or above
+        System.out.println("\nStudents who scored 60 or above:");
+        for (int i = 0; i < 6; i++) {
+            if (marks[i] >= 60) {
+                System.out.println(name[i] + " " + marks[i]);
+            }
+        }
+        scanner.close();
+    }
+}
